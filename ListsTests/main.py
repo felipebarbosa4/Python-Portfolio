9f2a4e77e2b52e1e3efb6622ad8285a8b56ad4b3
Felipe Figueira <felipe8figueira@gmail.com>
2023-05-31 16:47:01 -0400

    Tests-and-notes

---
+++ ListsTests/main.py
@@ -0,0 +1,71 @@
+fruits = [item1, item2]
+
+## Store all names of states US
+
+states_of_america = ["Delaware", "Penssylvania", "California", "Arkansas"]
+
+print(states_of_america[0])
+
+// output = Delaware
+
+print(states_of_america[-1]
+
+      // output = Arkansas
+
+States_of_america.append("Minas Gerais")
+
+print(states_of_america)
+
+// ["Delaware", "Penssylvania", "California", "Arkansas", "Minas Gerais"]
+
+list.remove(x) - will
+remove
+the
+first
+item
+from the list
+
+whose
+value is equal
+to
+x
+
+list.pop([i]) - remove
+the
+item
+at
+the
+given
+position in the
+list. if i is not specified
+it
+removes
+the
+last
+item in the
+list
+
+states_of_america.extend("Rio de Janeiro", "Sao Paulo")
+
+// ["Delaware", "Penssylvania", "California", "Arkansas", "Minas Gerais", "Rio de Janeiro", "Sao Paulo]
+
+
+
+
+    fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
+
+// What
+this
+prints?
+
+fruits = list[0]
+vegetables is list[1]
+
+dirty_dozen[1][1]
+[1] is vegetables
+[1] is kale
